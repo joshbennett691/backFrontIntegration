9d28c4f15407f645f2f7b770d29a80fd19af9c12
Josh <joshbennett010011@gmail.com>
2022-01-26 11:37:43 +0000

    Refactored controllers

---
+++ back/routes/bookRoute.js
@@ -0,0 +1,22 @@
+  express = require("express"),
+  router = express.Router();
+
+  const {createBook, getBook, getBooks, updateBook, deleteBook} = require('../controllers/bookController')
+
+// CREATE Book
+router.post("/create-book", createBook)
+
+// READ Books
+router.get("/", getBooks)
+
+// Get Single Book
+router.get("/edit-book/:id", getBook)
+
+// Update Book
+router.put("/update-book/:id", updateBook)
+
+// Delete Book
+router.delete("/delete-book/:id", deleteBook)
+
+module.exports = router;
+''
